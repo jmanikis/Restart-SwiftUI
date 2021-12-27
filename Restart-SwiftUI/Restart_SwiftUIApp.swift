ad5414aaf3df6e06c93baf2b370e16b2cef8194d
JC Manikis <jmanikis@icloud.com>
2021-12-27 18:02:22 -0500

    Initial Commit

---
+++ Restart-SwiftUI/Restart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUIApp.swift
+//  Restart-SwiftUI
+//
+//  Created by JC Manikis on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
